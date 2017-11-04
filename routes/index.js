8711fac4de0b4c5ecf1c03d32a82407f03e7a0a5
carlos <carlos.sigua@gmail.com>
2017-11-04 06:24:54 -0500

    Cambiando el tituio del index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'IOT 2017' });
+  res.render('index', { title: 'IOT 2017 Cuenca' });
 });
 
 
